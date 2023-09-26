411def9904afe6191d521e79eb8511c1cbce06bf
michael mbugua <mikembugua960@gmail.com>
2023-09-26 13:16:35 +0300

    replace import with require

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,25 +1,28 @@
-import express from 'express';
+const express = require("express");
 const router = express.Router();
 
+const getAll = (req, res) => {
+  res.send("Here are your items ");
+};
 
-export const getAll =(req,res)=>{
-    res.send("Here are your items ")
-}
+const getSingleTask = (req, res) => {
+  res.send("Here is your task ");
+};
 
-export const getSingleTask =(req,res)=>{
-    res.send("Here is your task ")
-}
+const createTasks = (req, res) => {
+  res.json(req.body);
+};
 
-export const createTasks = (req,res) =>{
-    res.json(req.body)
-}
-
-export const updateTasks = (req,res) =>{
-    res.send("update a task")
-}
-export const deleteTasks = (req,res) =>{
-    res.send("delete a task")
-}
-
-
-
+const updateTasks = (req, res) => {
+  res.send("update a task");
+};
+const deleteTasks = (req, res) => {
+  res.send("delete a task");
+};
+module.exports = {
+  getAll,
+  getSingleTask,
+  createTasks,
+  updateTasks,
+  deleteTasks,
+};
