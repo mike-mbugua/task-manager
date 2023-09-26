9ffec212ce83a733657905e071a44d92a15e86cf
michael mbugua <mikembugua960@gmail.com>
2023-09-26 15:24:42 +0300

    make the app listen to port

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 const express = require("express");
 const dotenv = require("dotenv");
+dotenv.config();
 const tasks = require("./routes/tasks.js");
 const connectDB = require("./db/connection.js");
 
-dotenv.config();
 const app = express();
-
-app.use(express.json());
 const port = process.env.PORT || 3000;
+
 // the middleware
+app.use(express.json());
 
 app.use("/api/v1/tasks", tasks);
 
+// connecting to the database using the async function
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    console.log(`Server running on port : ${port}`);
+
+    app.listen(port, () => {
+      console.log(`Server running on port : ${port}`);
+    });
   } catch (error) {
     console.log(error);
   }
