a9d4b046cca71cb6047842cd2efd92090ff5eda4
michael mbugua <mikembugua960@gmail.com>
2023-09-26 11:04:15 +0300

    setting up the routes to be rendered when testing

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('Task Manager App')
+import express  from "express";
+import dotenv from 'dotenv';
+import { getAll,getSingleTask,deleteTasks,updateTasks,createTasks } from "./controllers/tasks.js";
+
+dotenv.config()
+const app = express();
+
+app.use(express.json())
+const port = process.env.PORT || 3000;
+// the middleware
+
+app.get('/api/v1/tasks',getAll);
+app.post('/api/v1/tasks',createTasks);
+app.get('/api/v1/tasks/:id',getSingleTask);
+app.put('/api/v1/tasks/:id',updateTasks);
+app.delete('/api/v1/tasks/:id',deleteTasks);
+
+// a simple route to check the server
+
+app.get('/hello',(req,res)=>{
+    res.send("Hello mike your server is running ....")
+})
+
+app.listen(port,()=>{
+    console.log(`Server running on port : ${port}`);
+})
